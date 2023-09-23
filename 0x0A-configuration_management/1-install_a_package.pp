adf6b000825142ce051a63bf33ddeba2df9fc1d9
Mary-Afrika <marygahar@gmail.com>
2023-09-23 00:50:58 -0700

    ALX

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-# installs puppet-lint package
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# create a manifest that kills a process named killmenow
-exec { 'Kill':
-  command => '/usr/bin/pkill killmenow',
-  onlyif  => '/usr/bin/pgrep killmenow'
-}
